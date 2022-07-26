356d2ae6bd3e6bc21ec3de21718348bcee84baa3
Dell <Dell@DESKTOP-JE6GC22>
2022-07-26 15:19:53 +0530

    This is my 3rd commit

--- src/com/java/demo/Demo1.java
+++ src/com/java/demo/Demo1.java
@@ -5,6 +5,11 @@ public class Demo1
 	public static void main(String[] args) 
 	{
 		System.out.println("HI");
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		
+		System.out.println(sum);
 	}
 
 }
--- src/com/java/demo/Sample2.java
+++ src/com/java/demo/Sample2.java
@@ -2,9 +2,15 @@ package com.java.demo;
 
 public class Sample2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+		System.out.println("HI");
+		int p=10;
+		int q=20;
+		int sub=p-q;
+		
+		System.out.println(sub);
 	}
 
 }
